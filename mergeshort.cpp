4da942e9ef46e58c3867003b009785085bb951e1
NurvyyMelon <adinnaufal9@gmail.com>
2026-04-02 15:03:39 +0700

    final intergration

--- mergeshort.cpp
+++ mergeshort.cpp
@@ -63,3 +63,17 @@ void input() {
     for (int x = low; x <= high; x++) {
         arr[x] = B[x];
     }
+    void output() {
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    input();
+    mergeSort(0, n - 1);
+    output();
+    return 0;
+}
